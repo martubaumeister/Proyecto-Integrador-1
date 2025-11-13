9431116bb90381a3ed94e4143e0ae2484a89f397
guadapaz1 <pazga@udesa.edu.ar>
2025-11-13 11:24:34 -0300

    fetch category

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("probando")
+fetch('https://dummyjson.com/products/category-list')
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    let category= document.querySelector(".category")
+    category.innerHTML= data
+})
+.catch(function(error){
+    console.log(error);
+})
