82904a1c81042038562aba9b1c0e07e3953ac42f
guadapaz1 <pazga@udesa.edu.ar>
2025-11-16 21:46:41 -0300

    API de la pagina principal y css

--- js/index.js
+++ js/index.js
@@ -26,3 +26,35 @@ fetch('https://dummyjson.com/products/category-list')
 .catch(function(error){
     console.log(error);
 })
+
+fetch('https://dummyjson.com/products')
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+    let seccion1= document.querySelector(".tarjetas-home1")
+    seccion1.style.display= "none"
+
+    let seccion2= document.querySelector(".tarjetas-home2")
+    seccion2.style.display= "none"
+
+
+    let contenido= document.querySelector(".contenido")
+    tarjetas= ""
+    for (let i=0; i<data.products.length;i++){
+    tarjetas= tarjetas + ` 
+    <article class="tarjeta-home">
+        <p class="titulo">${data.products[i].title}</p>
+        <p class="precio">Precio:${data.products[i].price}</p>
+        <a class="boton"  href="product.html">Ver detalle</a>
+      </article>`
+    
+    contenido.innerHTML= `<div class="cajita">Todos los productos</div>
+    <h2 class="seccion1">productos más vendidos</h2>` 
+    + tarjetas
+    }
+})
+.catch(function(error){
+    console.log(error)
+})
